e273943b0a7b4d6e0fdfa535181c90c11fa44c87
feastco <122202303006@mhs.dinus.ac.id>
2025-03-03 14:54:26 +0700

    03-03-25

--- app/src/main/java/com/example/mobile2/ServerAPI.java
+++ app/src/main/java/com/example/mobile2/ServerAPI.java
@@ -2,5 +2,7 @@ package com.example.mobile2;
 
 public class ServerAPI
 {
-    public String BASE_URL = "http://192.168.1.9/webserver/";
+//    public String BASE_URL = "http://192.168.1.9/webserver/";
+    public String BASE_URL = "https://qgis.umrmaulana.my.id/webservice/";
+
 }
